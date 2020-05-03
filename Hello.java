092d4000fe47bf8313e6871e2f1d219e744c9cba
zameer603 <zameer.iiet@gmail.com>
2020-05-04 00:35:19 +0530

    add some lines

--- Hello.java
+++ Hello.java
@@ -3,5 +3,7 @@
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
+        System.out.println("Hello, Zameer!"); 
+        System.out.println("Hello, Mehjabi!"); 
     }
 }
